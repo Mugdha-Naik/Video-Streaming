7bec8b6d64432265c3a2d30470897c9c2606c19b
Mugdha-Naik <mugdhanaik63@gmail.com>
2025-08-30 18:39:40 +0530

    cloudinary and multer setup

--- src/models/video.models.js
+++ src/models/video.models.js
@@ -42,6 +42,6 @@ const videoSchema = new Schema(
 )
 
 // mongoose aggregation pipeline
-VidepSchema.plugin(mongooseAggregatePaginate)
+videoSchema.plugin(mongooseAggregatePaginate)
 
 export const Video = mongoose.model('Video', videoSchema);
